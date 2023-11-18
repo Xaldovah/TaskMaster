baf46ec35c8f665581af98cffe24118f32d3ae5c
Dennis Kimani <dxaldovah@gmail.com>
2023-11-18 00:03:02 -0800

    fixing indentations

--- app/models.py
+++ app/models.py
@@ -17,11 +17,9 @@ class Task(db.Model):
     priority = db.Column(db.String(20), nullable=True)
     status = db.Column(db.String(20), default='Incomplete')
     completed = db.Column(db.Boolean, default=False)
-    category_id = db.Column(
-        db.Integer, db.ForeignKey('category.id'), nullable=True)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-
 
     def __repr__(self):
         return f'<Task {self.title}>'
