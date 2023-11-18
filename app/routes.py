df99de51cd540b00e47e53f3221500ac82057874
Dennis Kimani <dxaldovah@gmail.com>
2023-11-18 04:20:38 -0800

    adde the updated at variable to the task class

--- app/routes.py
+++ app/routes.py
@@ -72,6 +72,7 @@ def get_tasks():
             'category_id': task.category_id,
             'user_id': task.user_id,
             'created_at': task.created_at.strftime('%Y-%m-%d %H:%M:%S')
+            'updated_at': task.updated_at.strftime('%Y-%m-%d %H:%M:%S')
         })
     return jsonify({'tasks': task_list})
 
