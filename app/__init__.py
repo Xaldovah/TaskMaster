0e62442c5582f905cad2576b4845814f3b3adb4e
xaldovah <dxaldovah@gmail.co>
2023-11-18 07:26:55 -0800

    minor changes to the flask

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 secret_key = secrets.token_urlsafe(32)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://xaldovah:@Denny23617@localhost/task_master'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://xaldovah:Denny23617@localhost/task_master'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = secret_key
 
