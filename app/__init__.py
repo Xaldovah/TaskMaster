c870a6faabf94453c2e606be54a2a90bb60ff921
xaldovah <dxaldovah@gmail.com>
2023-11-18 01:05:19 -0800

    migration completed sud successfully

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
@@ -8,5 +9,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://xaldovah:@Denny23617@localhost/
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from . import routes
