ff22b050cf6f80f5fc6e27f1bebc4ba647bd3449
Dennis Kimani <dxaldovah@gmail.com>
2023-11-18 01:09:34 -0800

    error handling for routes file

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
-from flask import jsonify, request, redirect, url_for
+from flask import jsonify, request, redirect, url_for, abort
 from app import app, db
-from app.models import *
+from app.models import Task
 
 
 @app.route('/api/tasks', methods=['GET'])
@@ -12,8 +12,7 @@ def get_tasks():
             'id': task.id,
             'title': task.title,
             'description': task.description,
-            'due_date': task.due_date.strftime(
-                '%Y-%m-%d') if task.due_date else None,
+            'due_date': task.due_date.strftime('%Y-%m-%d') if task.due_date else None,
             'priority': task.priority,
             'status': task.status,
             'category_id': task.category_id,
@@ -22,18 +21,27 @@ def get_tasks():
         })
     return jsonify({'tasks': task_list})
 
-
 @app.route('/api/tasks', methods=['POST'])
 def create_task():
     data = request.get_json()
-    new_task = Task(
-        title=data['title'],
-        description=data.get('description'),
-        due_date=data.get('due_date'),
-        priority=data.get('priority'),
-        category_id=data.get('category_id'),
-        user_id=data['user_id']
-    )
-    db.session.add(new_task)
-    db.session.commit()
-    return redirect(url_for('get_tasks'))
+
+    required_fields = ['title', 'user_id']
+    if not all(field in data for field in required_fields):
+        return jsonify({'error': 'Missing required fields'}), 400
+
+    try:
+        new_task = Task(
+            title=data['title'],
+            description=data.get('description'),
+            due_date=data.get('due_date'),
+            priority=data.get('priority'),
+            category_id=data.get('category_id'),
+            user_id=data['user_id']
+        )
+        db.session.add(new_task)
+        db.session.commit()
+        return redirect(url_for('get_tasks'))
+
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({'error': str(e)}), 500
