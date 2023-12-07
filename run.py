87595f544ee1962d6048da35e76524e9181db3e6
xaldovah <dxaldovah@gmail.com>
2023-12-07 17:04:03 +0000

    changed the run app

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
+from flask import Flask
 from app import app
 
+app = Flask(__name__, template_folder='templates')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run()
