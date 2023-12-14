b73a121829854274c6058f591f03e4452c7ea4b6
xaldovah <dxaldovah@gmail.co>
2023-12-14 09:10:00 -0800

    fixed the redirections

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -39,25 +39,3 @@ loginForm.addEventListener('submit', function(event) {
       }
     });
 });
-
-// Function to handle logout
-function logout() {
-  fetch('/logout', {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${accessToken}`
-    }
-  }).then(response => response.json())
-    .then(data => {
-      if (data.message === 'Logout successful') {
-        localStorage.removeItem('access_token');
-        window.location.href = '/login';
-      } else {
-        alert(data.error);
-      }
-    })
-    .catch(error => {
-      console.error(error);
-      alert('Internal server error. Please try again later.');
-    });
-}
