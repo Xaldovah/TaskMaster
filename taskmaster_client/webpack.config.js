ce3b24010ae885148c06c7c7b088974b8589953c
Replit user <>
2023-12-16 14:50:29 +0000

    frontend client integrated using react

---
+++ taskmaster_client/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	devServer: {
+		contentBase: path.resolve(__dirname, 'dist'),
+		port: 3000,
+		open: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+		],
+	},
+};
