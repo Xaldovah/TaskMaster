7c8c000a2cb522a0ebf6b9a366893ed48ebfa3b3
xaldovah <dxaldovah@gmail.co>
2023-12-28 07:01:50 -0800

    storing tasks in the localstorage for now

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,94 +1,60 @@
-// Function to fetch tasks from the server
-function fetchTasks() {
-    // Retrieve the access token from the server
-    $.ajax({
-        url: '/login',
-        type: 'GET',
-        dataType: 'json',
-        success: function(data) {
-            // Use the retrieved access token for subsequent requests
-	    let accessToken = localStorage.getItem('access_token');
-            displayTasks(accessToken);
-        },
-        error: function(error) {
-            console.error('Error retrieving access token:', error);
-        }
-    });
+function logout() {
+    localStorage.removeItem("email");
+    window.location.href = "/login";
 }
 
-// Function to display tasks in the UI
-function displayTasks(accessToken) {
-    $.ajax({
-        url: '/tasks',
-        type: 'GET',
-        dataType: 'json',
-        headers: {
-            'Authorization': 'Bearer ' + accessToken,
-        },
-        success: function(data) {
-            var taskList = $('#task-list');
-            taskList.empty(); // Clear existing tasks
+function createTask() {
+    // Fetch values from the form
+    const title = document.getElementById('title').value;
+    const description = document.getElementById('description').value;
+    const dueDate = document.getElementById('due_date').value;
+    const priority = document.getElementById('priority').value;
+    const status = document.getElementById('status').value;
 
-            data.tasks.forEach(function(task) {
-                var listItem = $('<li></li>');
-                listItem.html(`
-                    <span>Title:</span> ${task.title}<br>
-                    <span>Description:</span> ${task.description}<br>
-                    <span>Due Date:</span> ${task.due_date}<br>
-                    <span>Priority:</span> ${task.priority}<br>
-                    <span>Status:</span> ${task.status}<br>
-                `);
-                taskList.append(listItem);
-            });
-        },
-        error: function(error) {
-            console.error('Error fetching tasks:', error);
-        }
-    });
-}
-
-// Submit the form to create a new task
-$('#create-task-form').submit(function(event) {
-    event.preventDefault();
-
-    var formData = {
-        title: $('#title').val(),
-        description: $('#description').val(),
-        due_date: $('#due_date').val(),
-        priority: $('#priority').val(),
-        status: $('#status').val()
+    // Create a task object
+    const newTask = {
+        title: title,
+        description: description,
+        due_date: dueDate,
+        priority: priority,
+        status: status
     };
 
-    // Retrieve the access token from the server
-    $.ajax({
-        url: '/tasks',
-        type: 'GET',
-        dataType: 'json',
-        success: function(data) {
-            // Use the retrieved access token for the create task request
-            let accessToken = localStorage.getItem('access_token');
+    // Retrieve existing tasks from localStorage
+    const existingTasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
-            $.ajax({
-                url: '/tasks',
-                type: 'POST',
-                dataType: 'json',
-                headers: {
-                    'Authorization': 'Bearer ' + accessToken,
-                    'Content-Type': 'application/json'
-                },
-                data: JSON.stringify(formData),
-                success: function(response) {
-                    alert(response.message);
-                    // Refresh the task list
-                    fetchTasks();
-                },
-                error: function(error) {
-                    console.error('Error creating task:', error);
-                }
-            });
-        },
-        error: function(error) {
-            console.error('Error retrieving access token:', error);
-        }
+    // Add the new task to the array
+    existingTasks.push(newTask);
+
+    // Save the updated tasks array to localStorage
+    localStorage.setItem('tasks', JSON.stringify(existingTasks));
+
+    // Refresh the task list
+    fetchAndRenderTasks();
+}
+
+// Function to fetch tasks from localStorage and render them on the page
+function fetchAndRenderTasks() {
+    const tasksList = document.getElementById('tasksList');
+    tasksList.innerHTML = ''; // Clear existing tasks
+
+    // Retrieve tasks from localStorage
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+    // Render tasks on the page
+    tasks.forEach(task => {
+        const taskItem = document.createElement('li');
+        taskItem.innerHTML = `
+            <span>Title:</span> ${task.title}<br>
+            <span>Description:</span> ${task.description}<br>
+            <span>Due Date:</span> ${task.due_date}<br>
+            <span>Priority:</span> ${task.priority}<br>
+            <span>Status:</span> ${task.status}<br>
+            <hr>
+        `;
+        tasksList.appendChild(taskItem);
     });
-});
+}
+
+// Call the function when the page loads
+window.addEventListener('load', fetchAndRenderTasks);
